0b75f2d81b1252c3bdeb6b9033a7e04773405d1a
draugael <mdraugael@gmail.com>
2020-05-16 01:57:09 +0200

    first commit with everything

---
+++ bourse_twich.js
@@ -0,0 +1,24 @@
+
+
+jarEl.src = `https://uploads.twitchalerts.com/000/175/989/860/Bourse_outside.png`;
+
+cupBase[1] += 10;
+
+this.world.addBody(this.createRectangleBody([cupBase[0] - 145, cupBase[1] +375], 30, 30, 90));
+this.world.addBody(this.createRectangleBody([cupBase[0] - 135, cupBase[1] +338], 100, 30, -100));
+this.world.addBody(this.createRectangleBody([cupBase[0] - 175, cupBase[1] +260], 100, 30, -125));
+this.world.addBody(this.createRectangleBody([cupBase[0] - 210, cupBase[1] +190], 100, 30, -110));
+this.world.addBody(this.createRectangleBody([cupBase[0] - 220, cupBase[1] +100], 100, 30, -85));
+this.world.addBody(this.createRectangleBody([cupBase[0] - 205, cupBase[1] +50], 50, 30, -60));
+this.world.addBody(this.createRectangleBody([cupBase[0] - 160, cupBase[1] +15], 100, 30, -30));
+this.world.addBody(this.createRectangleBody([cupBase[0] - 80, cupBase[1] -10], 100, 30, -10));
+this.world.addBody(this.createRectangleBody([cupBase[0], cupBase[1]-15], 100, 30, 0));
+this.world.addBody(this.createRectangleBody([cupBase[0] + 80, cupBase[1] -10], 100, 30, 10));
+this.world.addBody(this.createRectangleBody([cupBase[0] + 160, cupBase[1] +15], 100, 30, 30));
+this.world.addBody(this.createRectangleBody([cupBase[0] + 205, cupBase[1] +50], 50, 30, 60));
+this.world.addBody(this.createRectangleBody([cupBase[0] + 220, cupBase[1] +100], 100, 30, 80));
+this.world.addBody(this.createRectangleBody([cupBase[0] + 215, cupBase[1] +170], 100, 30, 105));
+this.world.addBody(this.createRectangleBody([cupBase[0] + 180, cupBase[1] +260], 100, 30, 115));
+this.world.addBody(this.createRectangleBody([cupBase[0] + 160, cupBase[1] +338], 100, 30, 90));
+this.world.addBody(this.createRectangleBody([cupBase[0] + 160, cupBase[1] +338], 100, 30, 90));
+this.world.addBody(this.createRectangleBody([cupBase[0] + 180, cupBase[1] +370], 30, 30, 90));
